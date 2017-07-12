004ffb1d73a6f7460f2ca2eaa76f4dac2331e911
Yousef Ali Hassan <youssefali6212@gmail.com>
2017-07-12 13:11:13 +0200

    Add files via upload

---
+++ CodeForces/CF231_Div2_A_Teams.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n,ans = 0; cin>>n; //n: number of problems, ans: final answer
+    for(int i=0; i<n; i++){ //loop n times
+        int x,y,z; cin>>x>>y>>z; //the three members
+        ans += (x+y+z>=2);
+    }
+    cout<<ans<<endl;
+    return 0;
+}
