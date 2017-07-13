5e4d5f80a2ba23b945d212c3df0dd599ff5c1b88
Yousef Ali Hassan <youssefali6212@gmail.com>
2017-07-13 12:57:25 +0200

    codefights.com/challenge/uMQAhyc2uv8RfzbyH/

---
+++ CodeFights Challenges/gameOfSigns.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool recur(int i, int j, std::vector<std::string> m, char d){
+    if(d == 'U') for(int k=i-1; k>=0; k--){
+        if(m[k][j] == 'F')
+            return true;
+        if(m[k][j] == 'R' || m[k][j] == 'L')
+            return recur(k,j,m,m[k][j]);
+    }
+    if(d == 'D') for(int k=i+1; k<m.size(); k++){
+        if(m[k][j] == 'F')
+            return true;
+        if(m[k][j] == 'R' || m[k][j] == 'L')
+            return recur(k,j,m,m[k][j]=='R'? 'L':'R');
+    }
+    if(d == 'L') for(int k=j-1; k>=0; k--){
+        if(m[i][k] == 'F')
+            return true;
+        if(m[i][k] == 'R' || m[i][k] == 'L')
+            return recur(i,k,m,m[i][k]=='R'? 'U':'D');
+    }
+    if(d == 'R') for(int k=j+1; k<m[0].size(); k++){
+        if(m[i][k] == 'F')
+            return true;
+        if(m[i][k] == 'R' || m[i][k] == 'L')
+            return recur(i,k,m,m[i][k]=='R'? 'D':'U');
+    }
+    return false;
+}
+
+bool gameOfSigns(std::vector<std::string> m, std::string d) {
+    for(int i=0; i<m.size(); i++) for(int j=0; j<m[i].size(); j++)
+        if(m[i][j] == 'S')
+            return recur(i,j,m,d[0]);
+}
+
+int main(){
+    string arr[] = {"S-R",
+                    "F-R"};
+    vector<string> v(arr, arr+2);
+    cout<<gameOfSigns(v,"R"); // output : 1 (true)
+    return 0;
+}
