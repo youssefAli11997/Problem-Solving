88193c5bda92bcae03b741f2590b63208f0b5272
Youssef Ali <youssefali6212@gmail.com>
2017-07-30 00:59:21 +0200

    Update DerivativeSequence.cpp

--- Top Coder/DerivativeSequence.cpp
+++ Top Coder/DerivativeSequence.cpp
@@ -1,21 +1,21 @@
+// problem link : https://community.topcoder.com/stat?c=problem_statement&pm=6665
+
 #include <vector>
 using namespace std;
 
 class DerivativeSequence{
+	public:
 
-public:
+	vector<int> recur(vector<int> a, int n, int k){
+		if(n == 0) return a;
+		vector<int> b(k-1);
+		for(int i=0; i<k-1; i++)
+			b[i] = a[i+1] - a[i];
+		return recur(b, n-1, k-1);
+	}
 
-vector<int> recur(vector<int> a, int n, int k){
-	if(n == 0) return a;
-	vector<int> b(k-1);
-	for(int i=0; i<k-1; i++)
-		b[i] = a[i+1] - a[i];
-	return recur(b, n-1, k-1);
-}
-
-vector<int> derSeq(vector<int> a, int n){
-	int k = a.size();
-	return recur(a, n, k);
-}
-
+	vector<int> derSeq(vector<int> a, int n){
+		int k = a.size();
+		return recur(a, n, k);
+	}
 };
