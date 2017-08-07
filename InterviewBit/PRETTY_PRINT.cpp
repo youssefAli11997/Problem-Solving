293c54771cc3bb78418ee0c6bb748537e738d580
Youssef Ali <youssefali6212@gmail.com>
2017-08-07 21:49:20 +0200

    Update PRETTY_PRINT.cpp

--- InterviewBit/PRETTY_PRINT.cpp
+++ InterviewBit/PRETTY_PRINT.cpp
@@ -1,17 +1,28 @@
 // problem link : https://www.interviewbit.com/problems/prettyprint/
+/*  imput :
+    5
+    output :
+    5 5 5 5 5 5 5 5 5 
+    5 4 4 4 4 4 4 4 5 
+    5 4 3 3 3 3 3 4 5 
+    5 4 3 2 2 2 3 4 5 
+    5 4 3 2 1 2 3 4 5 
+    5 4 3 2 2 2 3 4 5 
+    5 4 3 3 3 3 3 4 5 
+    5 4 4 4 4 4 4 4 5 
+    5 5 5 5 5 5 5 5 5
+*/
 
 vector<vector<int> > res;
 
 void recur(int a, int b, int e){
     if(b > e) return;
-    for(int i = b; i<=e; i++)
+    for(int i = b; i<=e; i++){
         res[b][i] = a;
-    for(int i = b; i<=e; i++)
         res[i][b] = a;
-    for(int i = b; i<=e; i++)
         res[e][i] = a;
-    for(int i = b; i<=e; i++)
         res[i][e] = a;
+    }
     recur(a-1, b+1, e-1);
 }
 
