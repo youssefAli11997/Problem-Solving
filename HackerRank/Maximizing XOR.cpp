ebe8981a4ae3847527cc5063fcdc4c2e851bdc79
Youssef Ali <youssefali6212@gmail.com>
2017-08-07 12:45:51 +0200

    Create Maximizing XOR.cpp

---
+++ HackerRank/Maximizing XOR.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int mx = 0, l, r; cin>>l>>r;
+    for(int i=l; i<=r; i++) for(int j=i+1; j<=r; j++){
+        if((i ^ j) > mx) mx = i ^ j;
+    }
+    cout<<mx<<endl;
+    return 0;
+}
