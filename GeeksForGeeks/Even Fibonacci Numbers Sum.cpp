90aec86bf581ce4db2449f24f3c171026d981ed5
Youssef Ali <youssefali6212@gmail.com>
2017-08-11 22:15:09 +0200

    Update Even Fibonacci Numbers Sum.cpp

--- GeeksForGeeks/Even Fibonacci Numbers Sum.cpp
+++ GeeksForGeeks/Even Fibonacci Numbers Sum.cpp
@@ -4,22 +4,22 @@ using namespace std;
 
 int fib[35], n;
 
-int f(int x){
-    if(x <= 1)
-        return 1;
-    if(fib[x]) return fib[x];
-    return fib[x] = f(x - 1) + f(x - 2);
+int f(int x) {
+	if(x <= 1)
+	return 1;
+	if(fib[x]) return fib[x];
+	return fib[x] = f(x - 1) + f(x - 2);
 }
 
 int main() {
-    f(35); // generate fibonacci sequence
+	f(35); // generate fibonacci sequence
 	int t; cin>>t;
-	while(t--){
+	while(t--) {
 	    cin>>n;
 	    long long sum = 0;
 	    for(int i=0; fib[i] <= n; i++)
-	        if(fib[i] % 2 == 0)
-	            sum += fib[i];
+		if(fib[i] % 2 == 0)
+		    sum += fib[i];
 	    cout<<sum<<endl;
 	}
 	return 0;
