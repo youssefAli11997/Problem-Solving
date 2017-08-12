26054165da4ff66236d47d02d91acfb5e51dceae
Youssef Ali <youssefali6212@gmail.com>
2017-08-12 16:55:13 +0200

    Create Multiple Clocks.cpp

---
+++ AtCoder/Multiple Clocks.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long gcd(long long a, long long b){
+    if(b == 0) return a;
+    return gcd(b, a%b);
+}
+
+long long lcm(long long a, long long b){
+    return a / gcd(a,b) * b;
+}
+
+int main(){
+    int n; cin>>n;
+    long long a[n];
+    for(int i=0; i<n; i++){
+        cin>>a[i]; 
+    }
+    long long ans = a[0];
+    for(int i=1; i<n; i++){
+        ans = lcm(ans, a[i]);
+    }
+    cout<<ans<<endl;
+    return 0;
+}
