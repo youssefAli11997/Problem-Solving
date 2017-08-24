79bdeba79d47955f84cdcc070a6392666a2357a4
Youssef Ali <youssefali6212@gmail.com>
2017-08-24 12:32:58 +0300

    Update CF845A_Educ - Chess Tourney.cpp

--- CodeForces/CF845A_Educ - Chess Tourney.cpp
+++ CodeForces/CF845A_Educ - Chess Tourney.cpp
@@ -6,10 +6,7 @@ int main(){
     int a[2*n];
     for(int i=0; i<2*n; i++) cin>>a[i];
     sort(a, a+2*n);
-    for(int i=0; i<n; i++) for(int j=n; j<2*n; j++) if(a[i] == a[j]){
-        cout<<"NO\n";
-        return 0;
-    }
-    cout<<"YES\n";
+    if(a[n] == a[n-1]) cout<<"NO\n";
+    else cout<<"YES\n";
     return 0;
 }
