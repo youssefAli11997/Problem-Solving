1875d566072146f09d09f73e87877ad2ac823610
Youssef Ali <youssefali6212@gmail.com>
2017-08-29 13:26:58 +0300

    Create Simple problem?.cpp

---
+++ e-olymp/Simple problem?.cpp
@@ -0,0 +1,9 @@
+// problem link : https://www.e-olymp.com/en/problems/1
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+   int n; cin>>n;
+   cout<<n/10%10<<" "<<n%10<<endl;
+   return 0;
+}
