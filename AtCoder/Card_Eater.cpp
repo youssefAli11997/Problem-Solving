78260fcee8ed4fac7e828e648fc5bb54ff56cc4c
Youssef Ali <youssefali6212@gmail.com>
2017-09-11 02:04:31 +0300

    Update Card_Eater.cpp

--- AtCoder/Card_Eater.cpp
+++ AtCoder/Card_Eater.cpp
@@ -1,24 +1,17 @@
 // link : http://abc053.contest.atcoder.jp/tasks/arc068_b
 #include<bits/stdc++.h>
 using namespace std;
- int main()
- {
-     int n;
-     int a[200000];
-     cin>>n;
-     for(int i=0;i<n;i++)
-     {
-         cin>>a[i];
-     }
-     sort(a,a+n);
-     int x=unique(a,a+n)-a;
-     if(x%2)
-     {
-         cout<<x;
-     }
-     else
-     {
-         cout<<x-1;
-     }
-     return 0;
+int main(){
+    int n;
+    int a[200000];
+    cin>>n;
+    for(int i=0;i<n;i++)
+        cin>>a[i];
+    sort(a,a+n);
+    int x=unique(a,a+n)-a;
+    if(x%2)
+        cout<<x;
+    else
+        cout<<x-1;
+    return 0;
 }
