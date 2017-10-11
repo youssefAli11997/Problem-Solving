3b9b697dff2bab75413a2b582f7146a9a79f5e60
Youssef Ali <youssefali6212@gmail.com>
2017-10-11 13:05:40 +0200

    Create P2 - ACB Round 1.cpp

---
+++ A2OJ Code Battle 2017/Round 1/P2 - ACB Round 1.cpp
@@ -0,0 +1,26 @@
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define name first
+#define points second
+
+int main(){
+    int t; cin>>t;
+    while(t--){
+        int n,m; cin>>n>>m;
+        int a[n],ans=0;
+        for(int i=0; i<n; i++) cin>>a[i];
+        sort(a,a+n);
+        reverse(a,a+n);
+        for(int i=0; i<m; i++){
+            if(a[i] > 0)
+                ans++;
+        }
+        for(int i=m; i<n; i++) if(a[i] == a[m-1] && a[i] > 0)
+            ans++;
+        cout<<ans<<endl;
+    }
+    return 0;
+}
