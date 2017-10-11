1b4e70dd3baab0e61f64c6f576e10b10d0979f1e
Youssef Ali <youssefali6212@gmail.com>
2017-10-11 13:06:32 +0200

    Create P3 - ACB Scoreboard.cpp

---
+++ A2OJ Code Battle 2017/Round 1/P3 - ACB Scoreboard.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define solved first
+#define timeP second.first
+#define id second.second
+
+bool comp(pair<int, pair<int, int> > a, pair<int, pair<int, int> > b){
+    if(a.solved == b.solved){
+        if(a.timeP == b.timeP){
+            return a.id < b.id;
+        }
+        return a.timeP < b.timeP;
+    }
+    return a.solved > b.solved;
+}
+
+int main(){
+    int t; cin>>t;
+    while(t--){
+        int c,p,s; cin>>c>>p>>s;
+        int sub[c+2][p+2];
+        pair<int, pair<int, int> > conts[c];
+        for(int i=0; i<c; i++){
+            conts[i].id = i+1;
+            conts[i].solved = 0;
+            conts[i].timeP = 0;
+        }
+        memset(sub, 0, sizeof sub);
+        pair<pair<int,int>, pair<int,int> > qs[s];
+        for(int i=0; i<s; i++){
+            cin>>qs[i].first.first>>qs[i].first.second
+               >>qs[i].second.first>>qs[i].second.second;
+        }
+        sort(qs,qs+s);
+        for(int i=0; i<s; i++){
+            int t,cid,pid,r;
+            t = qs[i].first.first;
+            cid = qs[i].first.second;
+            pid = qs[i].second.first;
+            r = qs[i].second.second;
+            cid--; pid--;
+            if(r == 0 && sub[cid][pid] != -1){
+                sub[cid][pid]++;
+            }else if(r == 1 && sub[cid][pid] != -1){
+                conts[cid].timeP += 20 * sub[cid][pid] + t;
+                sub[cid][pid] = -1;
+                conts[cid].solved++;
+            }
+        }
+        
+        sort(conts, conts+c, comp);
+        for(int i=0; i<c; i++)
+            cout<<conts[i].id<<" "<<conts[i].solved<<" "<<conts[i].timeP<<endl;
+    }
+    return 0;
+}
