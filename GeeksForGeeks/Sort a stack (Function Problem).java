4b32c9e00266678d229bc0bd58cc4f44919a6c2d
Youssef Ali <youssefali6212@gmail.com>
2017-11-22 10:05:51 +0200

    Create Sort a stack (Function Problem).java

---
+++ GeeksForGeeks/Sort a stack (Function Problem).java
@@ -0,0 +1,16 @@
+class GfG{
+	public Stack<Integer> sort(Stack<Integer> s)
+	{
+		Stack<Integer> tmpStack = new Stack<>();
+		int x = 0;
+		
+		while(!s.isEmpty()){
+		    x = s.pop();
+		    while(!tmpStack.isEmpty() && tmpStack.peek() > x){
+		        s.push(tmpStack.pop());
+		    }
+		    tmpStack.push(x);
+		}
+		return tmpStack;
+	}
+}
