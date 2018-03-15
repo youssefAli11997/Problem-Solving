88636af01c45580fd9bf473d1728210673418289
Youssef Ali <youssefali6212@gmail.com>
2018-03-15 12:31:16 +0200

    Create Missing_in_arithmetic_progression.java

---
+++ TechGig/Missing_in_arithmetic_progression.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+public class CandidateCode {
+   public static void main(String args[] ) throws Exception {
+
+	    Scanner in = new Scanner(System.in);
+	    int n = in.nextInt(), first = 0, last = 0, x, actualSum = 0;
+	    for(int i=0; i<n; i++){
+	        x = in.nextInt();
+	        actualSum += x;
+	        if(i==0)
+	            first = x;
+	        if(i==n-1)
+	            last = x;
+	    }
+	    int trueSum = (n+1)*(first+last)/2;
+	    System.out.println(trueSum - actualSum);
+
+   }
+}
