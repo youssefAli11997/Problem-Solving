a29183a68d08eda7d75abc6b877f71279b959501
Youssef Ali <youssefali6212@gmail.com>
2018-03-15 12:56:50 +0200

    Update Display_Size.java

--- TechGig/Display_Size.java
+++ TechGig/Display_Size.java
@@ -1,3 +1,4 @@
+//link: https://www.techgig.com/practice/question/b3RUdk1RdFFjeGZ6bUg2ZTNVZXZBUT09
 public static String displaySize(int n){
   int minDiff = (int)1e7, rows = 0;
   for(int a=1; a*a<=n; a++) if(n % a == 0 && minDiff > (n/a)-a){
