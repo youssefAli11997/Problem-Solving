fd65a11977fad7119963be665e1ec9da6cc4c987
Youssef Ali <youssefali6212@gmail.com>
2018-04-09 16:38:06 +0200

    Update nDigits.cpp

--- CodeFights Challenges/nDigits.cpp
+++ CodeFights Challenges/nDigits.cpp
@@ -1,4 +1,5 @@
 //link: https://codefights.com/challenge/JFsYQuCRvwazY6kXw/
+//problem statement: How many digits does the number a^n contain?
 long long nDigits(long long n, long long a) {
     return n * log10(a) + 1;
 }
