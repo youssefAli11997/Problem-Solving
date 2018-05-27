f4737392862948d9db7162365edacac44336675c
Youssef Ali <youssefali6212@gmail.com>
2018-05-27 16:46:13 +0200

    Update and rename LongestDigitsPrefix.java to longestDigitsPrefix.java

--- CodeFights Challenges/longestDigitsPrefix.java
+++ CodeFights Challenges/longestDigitsPrefix.java
@@ -1,4 +1,3 @@
-//link: https://codefights.com/arcade/intro/level-9/r9azLYp2BDZPyzaG2/
 String longestDigitsPrefix(String inputString) {
     if(inputString.length() > 0 && inputString.charAt(0) >= '0' && inputString.charAt(0) <= '9')
         return inputString.charAt(0) + longestDigitsPrefix(inputString.substring(1));
