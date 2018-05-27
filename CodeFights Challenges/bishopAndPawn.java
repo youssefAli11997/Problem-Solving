811546a5650924a4a9428df5b655b5ff328144a5
Youssef Ali <youssefali6212@gmail.com>
2018-05-27 16:45:56 +0200

    Update and rename BishopAndPawn.java to bishopAndPawn.java

--- CodeFights Challenges/bishopAndPawn.java
+++ CodeFights Challenges/bishopAndPawn.java
@@ -1,4 +1,3 @@
-//link: https://codefights.com/arcade/intro/level-9/r9azLYp2BDZPyzaG2/
 boolean bishopAndPawn(String bishop, String pawn) {
     if(bishop.charAt(0) - pawn.charAt(0) == bishop.charAt(1) - pawn.charAt(1) ||
        bishop.charAt(0) - pawn.charAt(0) == -bishop.charAt(1) + pawn.charAt(1))
