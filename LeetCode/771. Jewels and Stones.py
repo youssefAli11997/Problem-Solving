dabd1a5b74156ea7349207dcc21d231549a8895d
Youssef Ali <youssefali6212@gmail.com>
2018-06-13 16:36:13 +0200

    Create 771. Jewels and Stones.py

---
+++ LeetCode/771. Jewels and Stones.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numJewelsInStones(self, J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+        cnt = 0
+        for c in S:
+            if c in J:
+               cnt += 1
+        return cnt
