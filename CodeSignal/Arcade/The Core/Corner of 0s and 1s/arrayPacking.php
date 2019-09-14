37f63cfba16b633761c69e8e9667e6e97e481dd3
Youssef Ali <youssefali6212@gmail.com>
2019-09-14 19:43:38 +0200

    Create arrayPacking.php

---
+++ CodeSignal/Arcade/The Core/Corner of 0s and 1s/arrayPacking.php
@@ -0,0 +1,8 @@
+//link: https://app.codesignal.com/arcade/code-arcade/corner-of-0s-and-1s/KeMqde6oqfF5wBXxf
+
+function arrayPacking($a) {
+    $m = 0;
+    for($i=0; $i<4; $i++)
+        $m += $a[$i] << (8 * $i);
+    return $m;
+}
