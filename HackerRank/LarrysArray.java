80b1363756915a82f262733d93f3eb5ae8290d3a
Youssef Ali <youssefali6212@gmail.com>
2019-09-18 14:14:39 +0200

    Create LarrysArray.java

---
+++ HackerRank/LarrysArray.java
@@ -0,0 +1,55 @@
+//link: https://www.hackerrank.com/challenges/larrys-array/problem
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class LarrysArray {
+
+    // Complete the larrysArray function below.
+    static String larrysArray(int[] A) {
+        int inversions = 0;
+        for(int i=0; i<A.length; i++)
+            for(int j=i+1; j<A.length; j++)
+                if(A[i] > A[j])
+                    inversions++;
+        return inversions % 2 == 0 ? "YES" : "NO";
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int[] A = new int[n];
+
+            String[] AItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < n; i++) {
+                int AItem = Integer.parseInt(AItems[i]);
+                A[i] = AItem;
+            }
+
+            String result = larrysArray(A);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
