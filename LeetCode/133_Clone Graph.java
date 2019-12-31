acd098f85014c8b1e3b3802e46778e8f993f5793
Youssef Ali <youssefali6212@gmail.com>
2019-12-31 19:28:34 +0200

    Create 133_Clone Graph.java

---
+++ LeetCode/133_Clone Graph.java
@@ -0,0 +1,32 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+
+    public Node() {}
+
+    public Node(int _val,List<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+class Solution {
+    Map<Node, Node> visited = new HashMap<>();
+    public Node cloneGraph(Node node) {
+        Node clonedNode = new Node(node.val, new ArrayList<Node>());
+        visited.put(node, clonedNode);
+        for(Node neigh : node.neighbors){
+            if(!visited.containsKey(neigh)){
+                Node clonedNeigh = cloneGraph(neigh);
+                visited.put(neigh, clonedNeigh);
+                clonedNode.neighbors.add(clonedNeigh);
+            }
+            else{
+                clonedNode.neighbors.add(visited.get(neigh));
+            }
+        }
+        return clonedNode;
+    }
+}
